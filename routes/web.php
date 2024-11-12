a84ee59a7b3a7c0fd22898b6e7eae8a8eb793fcc
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-11-12 20:27:33 -0300

    AB#53 feat(rota-login): Update rotas do login

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+
 Route::controller(LoginController::class)->group(function () {
     Route::get('login', 'index')->name('login.index');
     Route::post('login', 'store')->name('login.store');
