d0a9f6dff19e4068556eef84fd0aad3c7f7d5892
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-11-12 10:54:26 -0300

    AB#45 (controller-login): Criar controller login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth/login');
+    }
+
+    public function register()
+    {
+        return view('auth/register');
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect()->route('login.index');
+    }
+}
