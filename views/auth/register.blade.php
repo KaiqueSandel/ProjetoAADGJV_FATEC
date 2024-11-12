ab9c392607fe7e818677f9a7475a3804e340be3c
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-11-11 22:32:48 -0300

    AB#35 create(view-login): Criado view do registro

---
+++ views/auth/register.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+        <h2>Register</h2>
+        <hr class="mt-4 mb-5">
+</div>
+
+@endsection
