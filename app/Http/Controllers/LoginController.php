391aa0f1a59bcf7fdf43e9794d1a8197fcd450f0
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-11-12 14:02:07 -0300

    AB#45 feat(controller-login): Metodo store validar

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -47,6 +47,28 @@ class LoginController extends Controller
         return redirect()->route('login.index')->with('success', 'Registered successfully!');
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8']
+        ], [
+            'email.required' => 'E-mail is required.',
+            'email.email' => 'E-mail is invalid.',
+            'password.required' => 'Password is required.',
+            'password.min' => 'Password must have a minimun of :min characters!'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        $authenticated = Auth::attempt($credentials);
+
+        if (!$authenticated) {
+            return redirect()->route('login.index')->withErrors(['error' => 'E-mail or password are incorret!']);
+        }
+
+        return redirect()->route('home')->with('success', 'Logged in succesfully!');
+    }
+
     public function destroy()
     {
         Auth::logout();
