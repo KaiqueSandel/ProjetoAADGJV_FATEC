412761414ea78c2746f954b6e755410b377c100b
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-11-12 17:19:29 -0300

    AB#34 feat(view-login): Verificação logged

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,9 +4,18 @@
 
 @if (auth()->check())
 
-    <div>
-        <p>Loggin</p>
-        <p>Logged</p>
+<div class="container d-flex justify-content-center align-items-center w-50" style="min-height: 100vh">
+        <div class="col text-center">
+            <div class="row w-100 mb-5">
+                <p>
+                    You're already logged in as <span class="text-uppercase">{{ auth()->user()->first_name }}</span>
+                </p>
+            </div>
+            <div class="row w-100 justify-content-around">
+                <a href="{{ route('home') }}" class="btn btn-primary w-25">Go to homepage</a>
+                <a href="{{ route('login.destroy') }}" class="btn btn-danger w-25">Logout</a>
+            </div>
+        </div>
     </div>
 
 @endif
