98c91c200f3e0a3d8524e299c092c8e8ecdfb874
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-11-12 16:39:03 -0300

    Revert "KaiqueSandel-pull"

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::controller(LoginController::class)->group(function () {
-    Route::get('login', 'index')->name('login.index');
-    Route::post('login', 'store')->name('login.store');
-    Route::get('register', 'register')->name('login.register');
-    Route::post('register', 'create')->name('login.create');
-    Route::get('logout', 'destroy')->name('login.destroy');
+Route::get('/', function () {
+    return view('welcome');
 });
