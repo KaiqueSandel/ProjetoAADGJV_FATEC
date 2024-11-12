70aaba87165d453ade8ff36eeb59298adf5ffcf4
Vanderlei Zerbinati <114303351+VanzBits@users.noreply.github.com>
2024-11-12 20:23:15 -0300

    AB#51 criando model produto

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<float, int, string>
+     */
+    protected $fillable = [
+        'name',
+        'brand',
+        'description',
+        'price',
+        'category_id',
+    ];
+
+}
