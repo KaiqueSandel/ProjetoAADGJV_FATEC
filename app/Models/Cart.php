ec38a30722448b17d4629e7c70efe9fc6f37347a
Gabriel Meredija <114880504+GabrielMerd@users.noreply.github.com>
2024-11-13 14:49:33 -0300

    AB#48 feat(MODEL 1 - CARRINHO): Update da Model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -7,5 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, bool>
+     */
+    protected $fillable = [
+        'user_id',
+        'is_active',
+    ];
+
+    // Relationship with User (a cart belongs to a user)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relationship with CartItem (a cart has many items)
+    public function products()
+    {
+        return $this->hasMany(CartProduct::class);
+    }
+
+    public function hasProduct(string $productId)
+    {
+        $cartProduct = $this->products()->where('product_id', $productId)->first();
+        return $cartProduct ? $cartProduct->quantity : 0;
+    }
+
+    public function cartTotal()
+    {
+        return $this->products()->get()->sum(function (CartProduct $item): float {
+            return $item->price * $item->quantity;
+        });
+    }
 }
