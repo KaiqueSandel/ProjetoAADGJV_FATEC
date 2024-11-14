156bd48227d1320c9a0ce17ef8482964e5afb920
BitZerone <allyson.oliveira1303@gmail.com>
2024-11-14 11:34:07 -0300

    AB#42 feat(view-layout): implemento do produtos e categorias na navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,3 +11,37 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
 </head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active fs-5" aria-current="page" href="{{ route('home') }}">E-COMMERCE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active fs-5" aria-current="page"
+                            href="{{ route('products.getAll') }}">produtos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            categorias
+                        </a>
+                        <ul class="dropdown-menu">
+                            @foreach ($categories as $category)
+                                <li><a class="dropdown-item fs-5"
+                                        href="{{ route('products.byCategoryId', ['id' => $category->id]) }}">{{ $category->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
+
+
+                </ul>
+            </div>
