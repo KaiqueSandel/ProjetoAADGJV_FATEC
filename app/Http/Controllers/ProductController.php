05738538618af7708e20fb96ee4e30e1039bbf1f
Vanderlei Zerbinati <114303351+VanzBits@users.noreply.github.com>
2024-11-14 14:11:12 -0300

    .

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,19 +1,1 @@
-<?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Models\Category;
-use App\Models\Product;
-use App\Models\Cart;
-
-class ProductController extends Controller
-{
-    public readonly Product $product;
-
-    public function __construct()
-    {
-        $this->product = new Product();
-    }
-
-}
