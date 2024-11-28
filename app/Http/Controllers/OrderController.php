f958a85d63bb4488191a69170f10efeb9ca4e244
Vanderlei Zerbinati <114303351+VanzBits@users.noreply.github.com>
2024-11-28 20:39:53 -0300

    AB#77 finalizando order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -31,4 +31,52 @@ class OrderController extends Controller
     {
         return view('order/customer/order_show', ['order' => $order]);
     }
+
+    public function checkout(Request $request)
+    {
+        $user = auth()->user();
+        $cart = $user->cart();
+        $cartProducts = $cart->cartProducts;
+
+        if ($cartProducts->isEmpty()) {
+            return redirect()->back()->with(['error' => 'Your cart is empty!']);
+        }
+
+        // Validar o address_id vindo do formulário
+        $validator = Validator::make($request->all(), [
+            'address_id' => ['required', 'exists:addresses,id'],
+        ], [
+            'address_id.required' => "You need to select a shipping address!",
+            'address_id.exists' => "The given shipping address is invalid!",
+        ]);
+
+        if ($validator->fails()) {
+            // Pega o primeiro erro
+            $firstError = $validator->errors()->first();
+
+            // Redireciona com o primeiro erro
+            return redirect()->back()->with('error', $firstError);
+        }
+
+        // Criar a ordem
+        $order = Order::create([
+            'user_id' => $user->id,
+            'address_id' => $request->address_id, // Associa o endereço
+            'total' => $cart->cartTotal(),
+        ]);
+
+        foreach ($cartProducts as $cartProduct) {
+            OrderProduct::create([
+                'order_id' => $order->id,
+                'product_id' => $cartProduct->product->id,
+                'quantity' => $cartProduct->quantity,
+                'price' => $cartProduct->price,
+            ]);
+        }
+
+        // Limpar o carrinho
+        $cart->delete();
+
+        return redirect()->back()->with('success', 'Order placed successfully!');
+    }
 }
