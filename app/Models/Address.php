e02e40f0c2f2bca028fef70db763b46b7495444c
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-11-28 18:16:29 -0300

    Update Address.php

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -10,21 +10,4 @@ class Address extends Model
     /** @use HasFactory<\Database\Factories\AddressFactory> */
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'street',
-        'number',
-        'city',
-        'state',
-        'user_id',
-    ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
